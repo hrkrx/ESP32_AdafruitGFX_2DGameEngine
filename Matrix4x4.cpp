51f3c4fd3aeb33c8c9653b51d4f36dde01397f32
Hrkrx <sebastian.leifeld@web.de>
2018-10-30 12:03:01 +0100

    Matrix * Vector operator added

--- Matrix4x4.cpp
+++ Matrix4x4.cpp
@@ -2,5 +2,21 @@
 
 Vector4 Matrix4x4::operator*(const Vector4& other)
 {
-    
+    float x;
+    float y;
+    float z;
+    float w;
+
+    x = firstColumn.X * other.X + secondColumn.X * other.Y + thirdColumn.X * other.Z + fourthColumn.X * other.W;
+    y = firstColumn.Y * other.X + secondColumn.Y * other.Y + thirdColumn.Y * other.Z + fourthColumn.Y * other.W;
+    z = firstColumn.Z * other.X + secondColumn.Z * other.Y + thirdColumn.Z * other.Z + fourthColumn.Z * other.W;
+    w = firstColumn.W * other.X + secondColumn.W * other.Y + thirdColumn.W * other.Z + fourthColumn.W * other.W;
+
+    Vector4 result = *new Vector4();
+    result.X = x;
+    result.Y = y;
+    result.Z = z;
+    result.W = w;
+
+    return result;
 }
--- Matrix4x4.h
+++ Matrix4x4.h
@@ -3,6 +3,10 @@
  class Matrix4x4
  {
     public:
+    Vector4 firstColumn;
+    Vector4 secondColumn;
+    Vector4 thirdColumn;
+    Vector4 fourthColumn;
     float Get(int i, int j);
     Vector4 operator*(const Vector4& other);
     private:
